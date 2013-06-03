a6bab44c998e6f5d05fbb4983b769cb1e945d43c
David Alexander <dalexander@pacificbiosciences.com>
2013-06-03 07:16:15 -0800

    Fix for jenkins build failures in ConsensusCore
    [Tracking bug 22690]
    
    [git-p4: depot-paths = "//depot/software/common/ConsensusCore/": change = 124130]

--- make/Config-linux.mk
+++ make/Config-linux.mk
@@ -11,7 +11,7 @@ SWIG_LIB	    ?=  $(shell $(SWIG) -swiglib)
 export SWIG_LIB
 
 BOOST_INCLUDE_PATH  ?=  $(PROJECT_ROOT)/../../ThirdParty/boost/boost_1_47_0/
-NUMPY_INCLUDE_PATH  ?=  /usr/include/python2.6
-PYTHON_SYS_INCLUDE_PATH ?= /usr/include/python2.6
+NUMPY_INCLUDE_PATH  ?=  /usr/include/python2.7
+PYTHON_SYS_INCLUDE_PATH ?= /usr/include/python2.7
 
 PYTHON_SHLIB_FLAGS      ?= -pthread -shared -Wl,-O1
