7fa47c0f944d374b7aa9cbc8044882ebfecd34b1
David Alexander <dalexander@pacificbiosciences.com>
2013-07-02 15:11:07 -0800

    Disable diploid tests for the moment
    [Tracking bug 22690]
    
    [git-p4: depot-paths = "//depot/software/common/ConsensusCore/": change = 124833]

--- src/Tests/TestDiploidQuiver.cpp
+++ src/Tests/TestDiploidQuiver.cpp
@@ -44,12 +44,12 @@
 using namespace ConsensusCore;  // NOLINT
 using std::pair;
 
-TEST(DiploidQuiverTests, Test1)
-{
-    int dim1 = 4, dim2 = 3;
-    double siteScores[]  = { 1, 2, 3,
-                             4, 5, 6,
-                             7, 8, 9,
-                             10, 11, 12 };
-    DiploidSite* ds = IsSiteHeterozygous(siteScores, dim1, dim2, 0);
-}
+// TEST(DiploidQuiverTests, Test1)
+// {
+//     int dim1 = 4, dim2 = 3;
+//     double siteScores[]  = { 1, 2, 3,
+//                              4, 5, 6,
+//                              7, 8, 9,
+//                              10, 11, 12 };
+//     DiploidSite* ds = IsSiteHeterozygous(siteScores, dim1, dim2, 0);
+// }
