29c9ee4233d0fdb19f20d54369a0df1b60d6f4b1
Herb Maeder <hmaeder@pacificbiosciences.com>
2013-07-11 10:09:44 -0800

    consensus: bug 23171: fixing the primary build
    The assumption that the primary build did not call the csharp-pacbio
    target was incorrect, so the primary build was breaking trying to
    point to the new multi-os version of swig for the smrtanalysis build.
    
    We now detect if we are running a smrtanalysis build (by the existance
    of the software/smrtanalysis directory) and set the location of
    the swig binary and libs accordingly.
    
    [git-p4: depot-paths = "//depot/software/common/ConsensusCore/": change = 125017]

--- make/CSharp.mk
+++ make/CSharp.mk
@@ -1,15 +1,36 @@
 G_TOPDIR := ../..
-G_PBOUT_DIR := $(G_TOPDIR)/smrtanalysis/prebuilt.out
-G_BUILDOS_CMD := bash -c 'set -e; set -o pipefail; id=$$(lsb_release -si | tr "[:upper:]" "[:lower:]"); rel=$$(lsb_release -sr); case $$id in ubuntu) printf "$$id-%04d\n" $${rel/./};; centos) echo "$$id-$${rel%%.*}";; *) echo "$$id-$$rel";; esac'
-G_BUILDOS := 
-ifeq ($(UNAME), Linux)
-  G_BUILDOS := $(shell $(G_BUILDOS_CMD))
-endif
+G_SMRTANALYSIS_DIR=$(G_TOPDIR)/smrtanalysis
 
-G_SWIG_DIR := $(G_PBOUT_DIR)/swig/swig-2.0.10
-GA_SWIG_DIR := $(abspath $(G_SWIG_DIR))
-GA_SWIG_BINDIR := $(GA_SWIG_DIR)/$(G_BUILDOS)/bin
-GA_SWIG_LIBDIR := $(GA_SWIG_DIR)/$(G_BUILDOS)/share/swig/2.0.10
+# This makefile is shared between the smrtanlysis build and the dotnet (homer)
+# build.  But each is using a differen version of swig at the moment (and 
+# smrtanalysis needs to point to an os-specific version of swig). 
+# We will use the existance of the 'software/smrtanalysis' directory to 
+# determine if we are currently building smrtanalysis or dotnet (homer).  This
+# may not be 100% accurate, but it should work for release builds (and if a
+# dev build happens to be building dotnet (homer), but the smrtanalysis
+# directory exists, they will just pick up the new version of swiq)
+ifneq ($(wildcard $(G_SMRTANALYSIS_DIR)),)
+  # This is specific to the smrtanalysis build
+  G_PBOUT_DIR := $(G_SMRTANALYSIS_DIR)/prebuilt.out     
+G_BUILDOS_CMD := bash -c 'set -e; set -o pipefail; id=$$(lsb_release -si | tr "[:upper:]" "[:lower:]"); rel=$$(lsb_release -sr); case $$id in ubuntu) printf "$$id-%04d\n" $${rel/./};; centos) echo "$$id-$${rel%%.*}";; *) echo "$$id-$$rel";; esac'
+  G_BUILDOS :=                                                                  
+  ifeq ($(UNAME), Linux)
+    G_BUILDOS := $(shell $(G_BUILDOS_CMD))
+  endif                                   
+
+  G_SWIG_DIR := $(G_PBOUT_DIR)/swig/swig-2.0.10
+  G_SWIG_VERSION := $(patsubst swig-%,%,$(notdir $(G_SWIG_DIR)))
+  GA_SWIG_DIR    := $(abspath $(G_SWIG_DIR))                     
+  GA_SWIG_BINDIR := $(GA_SWIG_DIR)/$(G_BUILDOS)/bin
+  GA_SWIG_LIBDIR := $(GA_SWIG_DIR)/$(G_BUILDOS)/share/swig/$(G_SWIG_VERSION)
+else                                                                        
+  # This is specific to the dotnet (homer) build
+  G_SWIG_DIR     := ../../prebuilt.out/tools/swig-2.0.4
+  G_SWIG_VERSION := $(patsubst swig-%,%,$(notdir $(G_SWIG_DIR)))
+  GA_SWIG_DIR    := $(abspath $(G_SWIG_DIR))                     
+  GA_SWIG_BINDIR := $(GA_SWIG_DIR)/bin      
+  GA_SWIG_LIBDIR := $(GA_SWIG_DIR)/share/swig/$(G_SWIG_VERSION)
+endif         
 
 CSHARP_COMPILE           := gmcs -nowarn:0114
 CSHARP_LINK_NATIVE_FLAGS := -shared -static-libgcc -lstdc++ 
