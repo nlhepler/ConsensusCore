2e0e499834b2a1f9b7a07806b01f55ce8b667a36
Herb Maeder <hmaeder@pacificbiosciences.com>
2013-07-11 10:57:00 -0800

    consensus: bug 23171: fixing whitespace
    A copy/paste inserted whitespace at the end of some make variables
    causing the build to fail.  Removing all whitespace at the 
    end of variable assignments.
    
    [git-p4: depot-paths = "//depot/software/common/ConsensusCore/": change = 125022]

--- make/CSharp.mk
+++ make/CSharp.mk
@@ -11,26 +11,26 @@ G_SMRTANALYSIS_DIR=$(G_TOPDIR)/smrtanalysis
 # directory exists, they will just pick up the new version of swiq)
 ifneq ($(wildcard $(G_SMRTANALYSIS_DIR)),)
   # This is specific to the smrtanalysis build
-  G_PBOUT_DIR := $(G_SMRTANALYSIS_DIR)/prebuilt.out     
+  G_PBOUT_DIR := $(G_SMRTANALYSIS_DIR)/prebuilt.out
 G_BUILDOS_CMD := bash -c 'set -e; set -o pipefail; id=$$(lsb_release -si | tr "[:upper:]" "[:lower:]"); rel=$$(lsb_release -sr); case $$id in ubuntu) printf "$$id-%04d\n" $${rel/./};; centos) echo "$$id-$${rel%%.*}";; *) echo "$$id-$$rel";; esac'
-  G_BUILDOS :=                                                                  
+  G_BUILDOS :=
   ifeq ($(UNAME), Linux)
     G_BUILDOS := $(shell $(G_BUILDOS_CMD))
-  endif                                   
+  endif
 
   G_SWIG_DIR := $(G_PBOUT_DIR)/swig/swig-2.0.10
   G_SWIG_VERSION := $(patsubst swig-%,%,$(notdir $(G_SWIG_DIR)))
-  GA_SWIG_DIR    := $(abspath $(G_SWIG_DIR))                     
+  GA_SWIG_DIR    := $(abspath $(G_SWIG_DIR))
   GA_SWIG_BINDIR := $(GA_SWIG_DIR)/$(G_BUILDOS)/bin
   GA_SWIG_LIBDIR := $(GA_SWIG_DIR)/$(G_BUILDOS)/share/swig/$(G_SWIG_VERSION)
-else                                                                        
+else
   # This is specific to the dotnet (homer) build
   G_SWIG_DIR     := ../../prebuilt.out/tools/swig-2.0.4
   G_SWIG_VERSION := $(patsubst swig-%,%,$(notdir $(G_SWIG_DIR)))
-  GA_SWIG_DIR    := $(abspath $(G_SWIG_DIR))                     
-  GA_SWIG_BINDIR := $(GA_SWIG_DIR)/bin      
+  GA_SWIG_DIR    := $(abspath $(G_SWIG_DIR))
+  GA_SWIG_BINDIR := $(GA_SWIG_DIR)/bin
   GA_SWIG_LIBDIR := $(GA_SWIG_DIR)/share/swig/$(G_SWIG_VERSION)
-endif         
+endif
 
 CSHARP_COMPILE           := gmcs -nowarn:0114
 CSHARP_LINK_NATIVE_FLAGS := -shared -static-libgcc -lstdc++ 
